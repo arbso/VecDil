b820fec6ff3b84a9878df427d815f6734fbee580
arbso <pentaxonn@gmail.com>
2023-11-30 17:19:37 +0100

    login2

--- VecDil/Models/Login/LoginRequest.cs
+++ VecDil/Models/Login/LoginRequest.cs
@@ -5,11 +5,10 @@ namespace VecDil.Models
     {
         public LoginRequest()
         {
-            this.UserName = String.Empty;
+            this.Email = String.Empty;
             this.Password = String.Empty;
         }
 
-        public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public int Role { get; set; }
